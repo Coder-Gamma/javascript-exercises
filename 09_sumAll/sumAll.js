24701f345c6a81c16291149c6c9439723c7a02d3
Gamma <221785314+Coder-Gamma@users.noreply.github.com.>
2025-12-17 11:32:46 -0500

    It was fun

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
 
+   
+
+const sumAll = function(a,b) {
+ let total = 0;
+
+ if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+ if(a < 0 || b < 0) return "ERROR";
+    if(a>b){
+          while(b<=a) {
+        total = total + b;
+        b++;
+    }
+    return total;
+    }
+    else {
+  while(a<=b) {
+        total = total + a;
+        a++;
+    }
+    return total;
+    }
+  
 };
+
+const result = sumAll(10,4);
 
 // Do not edit below this line
 module.exports = sumAll;
