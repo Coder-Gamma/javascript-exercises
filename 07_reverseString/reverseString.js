443010bfe27f4bdb15148e3be00027ee99563909
Gamma <221785314+Coder-Gamma@users.noreply.github.com.>
2025-12-17 04:46:20 -0500

    done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArr = string.split("");
+    stringArr.reverse();
+    return stringArr.join("");
 };
-
+const result  = reverseString('Hello Javascript');
+console.log(result);
 // Do not edit below this line
 module.exports = reverseString;
