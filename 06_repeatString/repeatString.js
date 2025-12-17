c85614876a543d0fc96b441f3ff753c8cdf526b0
Gamma <221785314+Coder-Gamma@users.noreply.github.com.>
2025-12-17 04:25:59 -0500

    Too ezz

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
-
+    let newString = string;
+    if(num==0){
+        return "";
+    }
+    else if ( num < 0) {
+    return "ERROR";
+    }
+        
+    for(let i = 1; i < num; i++) {
+      newString += string;
+    }
+    return newString;
+}
+repeatString('Nirdosh',7);
 // Do not edit below this line
 module.exports = repeatString;
