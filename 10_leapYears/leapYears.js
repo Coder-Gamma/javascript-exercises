ccdf7f371be97d94402a20992d1454e8c593d6ee
Gamma <221785314+Coder-Gamma@users.noreply.github.com.>
2025-12-17 12:04:10 -0500

    done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+    if(year % 4 === 0) {
+        if(year >=100 && year%400 !==0) {
+            if(year%100 === 0 ) {
+            return false;
+        }
+         else if ( year % 400 === 0){
+            return true;
+         }
+        }
+       
+        return true;
+    }
+    else return false;
 
+} 
+
+console.log(leapYears(1984));
 // Do not edit below this line
 module.exports = leapYears;
