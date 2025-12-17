146ca7caefd336aa76a12c5679869acee74dbc0d
Gamma <221785314+Coder-Gamma@users.noreply.github.com.>
2025-12-17 12:17:29 -0500

    pretty cool

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+
+  const result = ((tempInF-32) * 5/9);
+
+  return Math.ceil(result *10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+
+  const result = (tempInC *9/5) + 32;
+    return Math.ceil(result *10)/10;
+
 };
+convertToCelsius(32);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
